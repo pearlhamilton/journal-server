4b67d87336e4fd8c79a85a6622fc2d2a202c7f0f
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-03-12 17:02:44 +0000

    add get and post routes and entry class

---
+++ models/entry.js
@@ -0,0 +1,25 @@
+const entriesData = require('../data');
+
+class Entry {
+    constructor(data) {
+        this.id = data.id;
+        this.message = data.message;
+        this.reacts = data.reacts;
+        this.comments = data.comments;
+    }
+
+
+    static get all(){
+        const data = entriesData.map(entry => new Entry(entry));
+        return data;
+    }
+
+    static create(data){
+        const newID = entriesData.length + 1;
+        const newEntry = new Entry({id: newID, ...data});
+        entriesData.push(newEntry);
+        return newEntry;
+    }
+}
+
+module.exports = Entry;
