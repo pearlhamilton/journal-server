14c314eeec38210889f14fa54ff4d8d2aec7a623
Elwin <elwin.carlos09@gmail.com>
2021-03-17 07:28:20 +0000

    update model to reflect changes in json manager

--- models/entry.js
+++ models/entry.js
@@ -5,7 +5,7 @@ class Entry {
     constructor(data) {
         this.id = data.id;
         this.message = data.message;
-        this.gif = data.gif;
+        // this.gif = data.gif;
         this.reacts = data.reacts || [0,0,0];
         this.comments = data.comments || [];
     }
@@ -17,9 +17,10 @@ class Entry {
 
     static findById(id) {
         const filteredEntry = jsonData.filter((entry) => entry.id===id)[0];
+        const selectedEntry = new Entry(filteredEntry);
         if(filteredEntry) {
-            console.log(`Hooray, entry ${id} already exists!`);
-            return filteredEntry;
+            console.log(`Hooray, entry ${id} already exists!`);            
+            return selectedEntry;
         } else {
             throw new Error(`${id} is not a valid entry ID.`)
         }
@@ -37,14 +38,14 @@ class Entry {
         if (data.comments.length > 0) {
             this.comments.push(...data.comments)
         }
-        // TODO - update json
+        jsonHelpers.addCommentsJSON(this);
         return this.comments;
     }
 
     updateReacts(data){    
         const reactIdx = parseInt(data.reactBtn) - 1;
         this.reacts[reactIdx] += 1;
-        // TODO - update json
+        jsonHelpers.addReactsJSON(this);
         return this.reacts;
     }
 
