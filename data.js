2c0ebc1da7ab146ca07058b38b47548544b0a5ce
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-03-17 11:29:05 +0000

    add mock date

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 const entries = [
     {  
         id: 1,
-        date: new Date(), 
+        date: new Date("Wed Mar 17 2021 10:23:05 GMT+0000 (Greenwich Mean Time)"), 
         message: "my first jurnool entry",
         // gif: "https://media.giphy.com/media/QMHoU66sBXqqLqYvGO/giphy.gif",
         reacts: [0,0,0],
