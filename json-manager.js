985f398757ecca3e5d7f111cb492236b5e3d0b87
Elwin <elwin.carlos09@gmail.com>
2021-03-17 07:27:16 +0000

    add methods for updating comments and reacts in JSON file

--- json-manager.js
+++ json-manager.js
@@ -20,11 +20,11 @@ function addToJSON(entry) {
             console.log(`Oops! Error reading file: ${err}`);
         } else {
             //parse JSON string into JSON object            
-            const entriesData = JSON.parse(data);
+            const jsonData = JSON.parse(data);
             //Push new data into file
-            entriesData.push(entry);
+            jsonData.push(entry);
             //write new data back into file
-            const jsonString = JSON.stringify(entriesData, null, 4)
+            const jsonString = JSON.stringify(jsonData, null, 4)
             fs.writeFile(filepath, jsonString, (err) => {
                 if (err) {
                     console.log(`Error writing file: ${err}`);
@@ -32,7 +32,55 @@ function addToJSON(entry) {
             });
             console.log("Successfully added file to JSON!");                
         }
-    })
+    })    
 }
 
-module.exports = { readJSON, addToJSON };
+//Create function that edits comments in json file
+function addCommentsJSON(entry) {
+    //Write file to disk
+    fs.readFile(filepath, 'utf8', (err, data) => {
+        if(err) {
+            console.log(`Oops! Error reading file: ${err}`);
+        } else {
+            //parse JSON string into JSON object            
+            const jsonData = JSON.parse(data);
+            //Identify entry that has a new comment
+            const changedEntry = jsonData.filter((original) => original.id===entry.id)[0];
+            //Push new comment to changed entry
+            changedEntry.comments.push(entry.comments.pop());
+            //write new data back into file
+            const jsonString = JSON.stringify(jsonData, null, 4)
+            fs.writeFile(filepath, jsonString, (err) => {
+                if (err) {
+                    console.log(`Error adding comment to file: ${err}`);
+                }
+            });
+            console.log(`Successfully added comment to entry ${entry.id} in JSON!`);                
+        }
+    })    
+}
+
+//Create function that adds reacts to json file
+function addReactsJSON(entry) {
+    //Write file to disk
+    fs.readFile(filepath, 'utf8', (err, data) => {
+        if(err) {
+            console.log(`Oops! Error reading file: ${err}`);
+        } else {
+            //parse JSON string into JSON object            
+            const jsonData = JSON.parse(data);
+            //Replace entry in JSON file with new entry withu updated reacts
+            jsonData[entry.id-1] = entry;
+            //write new data back into file
+            const jsonString = JSON.stringify(jsonData, null, 4)
+            fs.writeFile(filepath, jsonString, (err) => {
+                if (err) {
+                    console.log(`Error writing file: ${err}`);
+                }
+            });
+            console.log(`Successfully updated number of reacts for entry number ${entry.id}.`);                
+        }
+    })    
+}
+
+module.exports = { readJSON, addCommentsJSON, addReactsJSON, addToJSON };
